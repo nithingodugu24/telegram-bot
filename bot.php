76385f14d8fce1efef22a44053b7ad3fd2e64a5a
nithingodugu24 <85222889+nithingodugu24@users.noreply.github.com>
2022-10-08 10:52:57 +0530

    Update bot.php

--- bot.php
+++ bot.php
@@ -241,63 +241,6 @@ else {
     }
 
 
-//Wheather API
-if(strpos($message, "/attendance") === 0){
-        $location = substr($message, 9);
-        
-
-if( $location == ''){
-send_message($chat_id,$message_id, "Enter Pin Number");
-}else{
-
-$url = "https://exams.sbtet.telangana.gov.in/API/api/PreExamination/getAttendanceReport?Pin=".$location."";
-
-
-$curl = curl_init($url);
-curl_setopt($curl, CURLOPT_URL, $url);
-curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-
-//for debug only!
-curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
-curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-
-$resp = curl_exec($curl);
-curl_close($curl);
-
-$resp2 = substr($resp, 1, -1);
-
-$resp2 = str_replace("\\", "", $resp2);
-
-
-
-$mains = json_decode($resp2, true);
-
-
-$matt = $mains['Table'][0]['NumberOfDaysPresent'] /90*100;
- 
-
-
-$botmessage = 'Attendance Report : %0APin  : '.$mains['Table'][0]['Pin'].'%0AName : '.$mains['Table'][0]['Name'].'%0APresent Days : '.$mains['Table'][0]['NumberOfDaysPresent'].'%0AWeekly Attendance: '.$mains['Table'][0]['Percentage'].'%0AMain Attendance : '.round($matt).'';
-
-
-$matt = $mains['Table'][0]['NumberOfDaysPresent'] /90*100;
- 
-
-if($mains['Table'][0]['Name']!= ''){
-
-
-        send_MDmessage($chat_id,$message_id, "***
-Attendance of $location:
-Name: $mains['Table'][0]['Name']
-Present Days : $mains['Table'][0]['NumberOfDaysPresent']
-Weekly Attendance : $mains['Table'][0]['Percentage']
-Main Attendance : round($matt) 
-Checked By @$username ***");
-}
-else {
-           send_message($chat_id,$message_id, "Invalid Pin Number");
-}}
-    }
 
 
 ///Github User API
